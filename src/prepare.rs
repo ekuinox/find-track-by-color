2f837b9247fb1f939f53362c6b7a79b27464524e
ekuinox <depkey@me.com>
2022-12-07 00:33:30 +0900

    これで多少マシでは...

--- src/prepare.rs
+++ src/prepare.rs
@@ -1,29 +1,53 @@
 use anyhow::{bail, Result};
-use futures::{pin_mut, TryStreamExt};
+use futures::{pin_mut, StreamExt};
+use indicatif::ProgressBar;
 use rspotify::{
     model::{FullTrack, Image},
     prelude::{BaseClient, OAuthClient},
 };
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use tokio::io::{AsyncWriteExt, BufWriter};
 
 pub async fn prepare(client: impl BaseClient + OAuthClient, directory: PathBuf) -> Result<()> {
     let stream = client.current_user_saved_tracks(None);
     pin_mut!(stream);
-    println!("Items (blocking):");
 
     tokio::fs::create_dir_all(&directory).await?;
 
-    // 並列にやれるようにしたいね
-    while let Ok(Some(item)) = stream.try_next().await {
-        save_track_image(&directory, &item.track).await?;
-    }
+    let items = stream
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>();
+    let pb = Arc::new(ProgressBar::new(items.len() as u64));
+
+    let _ = futures::future::join_all(
+        items
+            .into_iter()
+            .map(|item| save_track_image_with_pb(&directory, item.track, pb.clone())),
+    )
+    .await;
 
     Ok(())
 }
 
+async fn save_track_image_with_pb(
+    directory: &Path,
+    track: FullTrack,
+    pb: Arc<ProgressBar>,
+) -> Result<()> {
+    let r = save_track_image(&directory, track).await;
+    pb.inc(1);
+    r?;
+    Ok(())
+}
+
 /// とりあえず画像を保存しまくる
-async fn save_track_image(directory: &Path, track: &FullTrack) -> Result<()> {
+async fn save_track_image(directory: &Path, track: FullTrack) -> Result<()> {
     let Some(Image { url, .. }) = track.album.images.first() else { bail!("") };
     let Some(track_id) = &track.id else { bail!("") };
     let bytes = reqwest::get(url).await?.bytes().await?;
